f0e4c67d50bad69333d391972d97964256649bfb
Flash <Flash@localhost.localdomain>
2012-03-20 23:22:17 +0400

    some improvements;

--- CheckFile.py
+++ CheckFile.py
@@ -2,13 +2,16 @@
 
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
+from Functions import USER_UID, USER_GID
 import os, stat, os.path
 
 def wrapData(args):
 	return \
-	'<pre>Size: %s : %s<pre>Mode: %s : %s<pre>User: %s : %s<pre>Group: %s : %s<pre>Mtime: %s : %s' \
+	'<pre>RealData : PackageData\
+	<pre>Size: %s : %s<pre>Mode: %s : %s<pre>User: %s : %s<pre>Group: %s : %s<pre>Mtime: %s : %s' \
 	% (args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9]) + \
-	('<pre>Link:\t%s<pre>\t%s' % (args[10], args[11]) if (args[10], args[11])!=('', '') else '')
+	('<pre>Link:\t%s<pre>\t%s' % (args[10], args[11]) if (args[10], args[11])!=('', '') else '') + \
+	('<pre>WARNING: not unique data in rpmDB (%s records)' % args[12] if int(args[12])>1 else '')
 
 class CheckFile(QWidget):
 	def __init__(self, parent = None):
@@ -33,6 +36,17 @@ class CheckFile(QWidget):
 		self.layout.addItem(self.pathLayout, 0, 0, 2, 3)
 
 		self.package = QLabel('Package :')
+
+		self.mode = QComboBox()
+		self.mode.setIconSize(QSize(32,32))
+		self.mode.addItem (QIcon('/usr/share/thrifty/icons/user.png'), '')
+		self.mode.addItem (QIcon('/usr/share/thrifty/icons/admin.png'), '')
+		self.mode.setToolTip('User Mode')
+		self.start = QPushButton(QIcon('/usr/share/thrifty/icons/start.png'), '')
+		self.start.setIconSize(QSize(32,32))
+		self.start.clicked.connect(self.checkFile)
+		self.start.setToolTip('Start task')
+
 		self.packageCheckSumm = QLabel('Package CheckSumm :')
 		self.checkSumm = QLabel('Real CheckSumm :')
 		self.packageRes = QLabel('')
@@ -40,6 +54,8 @@ class CheckFile(QWidget):
 		self.checkSummRes = QLabel('')
 		self.otherData = QLabel('')
 		self.layout.addWidget(self.package, 3, 0)
+		self.layout.addWidget(self.mode, 4, 0, Qt.AlignLeft)
+		self.layout.addWidget(self.start, 4, 0, Qt.AlignRight)
 		self.layout.addWidget(self.packageCheckSumm, 5, 0)
 		self.layout.addWidget(self.checkSumm, 6, 0)
 		self.layout.addWidget(self.packageRes, 3, 1)
@@ -48,7 +64,14 @@ class CheckFile(QWidget):
 		self.layout.addWidget(self.checkSummRes, 6, 1)
 
 		self.setLayout(self.layout)
+		self.mode.currentIndexChanged.connect(self.changeMode)
 		self.setMinimumSize(32, 32)
+
+	def changeMode(self, i = 0):
+		if i :
+			self.mode.setToolTip('Root Mode')
+		else :
+			self.mode.setToolTip('User Mode')
 
 	def addPath(self):
 		fileName = QFileDialog.getOpenFileName(self, 'Path_to_', '~')
@@ -67,15 +90,22 @@ class CheckFile(QWidget):
 		self.packageCheckSummRes.setText('')
 		self.checkSummRes.setText('')
 		self.otherData.setText('')
-		print self.pathString.text().toUtf8().data()
+		mode = 0 if self.mode.currentIndex() else 1
+		path = self.pathString.text().toUtf8().data()
+		if not os.access(path, os.R_OK) and mode == 1:
+			with open('/dev/shm/thrifty.lastTask', 'wb') as f :
+				f.write('package:Permissin denied.\nmulti:0')
+			self.showResult()
+			return
 		self.t = QProcess()
 		Data = QStringList()
 		Data.append('/usr/share/thrifty/thrifty.py')
-		opt = ''.join(('G:', '-/-', '::', '-f'))
+		opt = ''.join(('G:', str(USER_UID), '/', str(USER_GID), '::', '-f'))
 		Data.append(opt)
 		Data.append(self.pathString.text())
 		self.t.finished.connect(self.showResult)
-		self.t.start('python', Data)
+		if mode : self.t.start('python', Data)
+		else : self.t.start('pkexec', Data)
 		if self.t.waitForStarted() :
 			self.runned = True
 			#print self.t.state()
@@ -98,7 +128,7 @@ class CheckFile(QWidget):
 				_data = f.read()
 			os.remove(name_)
 			data = _data.split('\n')
-			STR = ['' for i in xrange(12)]
+			STR = ['' for i in xrange(13)]
 			for item in data :
 				_data = item.split(':')
 				if len(_data) > 1 :
@@ -132,6 +162,8 @@ class CheckFile(QWidget):
 						STR[10] = _data[1]
 					elif _data[0] == 'linkP' :
 						STR[11] = _data[1]
+					elif _data[0] == 'multi' :
+						STR[12] = _data[1]
 			self.otherData.setText(wrapData(STR))
 		else :
 			self.packageRes.setText('Error : not successfull.')
